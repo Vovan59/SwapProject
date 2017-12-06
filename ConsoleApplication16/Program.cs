fe493ea2440893379bcfa6a4328272aee11b19f1
User <User@K3-512-09>
2017-12-06 19:25:38 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 1;
+            Swap(a, b);
+
+            Console.WriteLine("a={0} b={1}", a, b);
+            Console.ReadLine();
+
+        }
+        /// <summary>
+        /// меняет значения переменных
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        private static void Swap(int a,  int b)
+        {       
+            int c = a;
+            a = b;
+            b = c;
+        }
+    }
+}
